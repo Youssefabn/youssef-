f0adb71095d34432cb4244454a21f63979bbc5b8
Youssefabn <103999296+Youssefabn@users.noreply.github.com>
2022-04-20 01:58:17 +0300

    Create DefCat.cpp

---
+++ DefCat.cpp
@@ -0,0 +1,14 @@
+#include<iostream>;
+#include " cat.h";
+#include "animal.h";
+Using std :: cout;
+Cat :: Cat (int n, int a, int pSize, int c):Animal(n, a){
+Cout<<" Cat Created/n";
+PawSize = pSize;
+Color = c;
+}
+
+Cat :: ~Cat(){
+Cout<<" Car Destroyed/n";
+}
+

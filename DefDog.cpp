7ad5b303c0bf29584d951d652e1bcca272d41963
Youssefabn <103999296+Youssefabn@users.noreply.github.com>
2022-04-20 01:40:59 +0300

    Update DefDog.cpp

--- DefDog.cpp
+++ DefDog.cpp
@@ -2,7 +2,7 @@
 #include " dog.h";
 #incluse " animal.h";
 Using std :: cout ;
-Dog :: Dog ( int n, int a, int b) : Animal
+Dog :: Dog ( int n, int a, int b) : Animal(n ,a) {
  Cout<<" Dog created/n" ;
 Breed = b ; } 
 Dog :: ~Dog(){

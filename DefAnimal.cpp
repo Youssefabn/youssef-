d3caa1aaad53bd3fa65db03c1dd1682c7584fc94
Youssefabn <103999296+Youssefabn@users.noreply.github.com>
2022-04-20 02:13:10 +0300

    Create DefAnimal.cpp

---
+++ DefAnimal.cpp
@@ -0,0 +1,11 @@
+#include<iostream>;
+#include "animal.h";
+Using std :: cout ;
+Animal :: Animal( int n , int a ){
+Cout <<"Animal Created/n";
+name = n ;
+age = a ;
+}
+Animal :: ~Animal() { 
+Cout <<" Animal Destroyed/n";
+} ; 

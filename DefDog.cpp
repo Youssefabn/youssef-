5d59e6a3e4a0c4077c8bdca26194f5919efac1ae
Youssefabn <103999296+Youssefabn@users.noreply.github.com>
2022-04-20 01:36:16 +0300

    Create DefDog.cpp

---
+++ DefDog.cpp
@@ -0,0 +1,9 @@
+#include<iostream>;
+#include " dog.h";
+#incluse " animal.h";
+Using std :: cout ;
+Dog :: Dog ( int n, int a, int b) : Animal
+ Cout<<" Dog created/n" ;
+Breed = b ; } 
+Dog :: ~Dog(){
+Cout<<" Dog Destroyed /n";}

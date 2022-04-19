7484e4b0a3a91591bd1455707cc554723a109b39
Youssefabn <103999296+Youssefabn@users.noreply.github.com>
2022-04-20 01:20:42 +0300

    Create Def fish.cpp

---
+++ Def fish.cpp
@@ -0,0 +1,11 @@
+#include<iostream>;
+#include"fish.h";
+using std :: cout ;
+Fish : : Fish(int n , int a , int gCap , int, sS):Animal(n,a){
+Cout <<"Fish created/n";
+gillCapacity = gcap;
+swimSpeed = swimSpeed;
+}
+Fish : : ~Fish( ) {
+Cout <<" Fish destroyed/n ";
+}
